91b0bf990aef9260897e3a4de003ce9587d5889a
QvikerTester <67345179+QvikerTester@users.noreply.github.com>
2025-01-27 19:31:38 +0400

    uiui

---
+++ NotBot2/Models/CardPaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotBot2.Models
+{
+    public class CardPaymentDetail
+    {
+        [Key]
+        public int PaymentID { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string HolderName { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode {  get; set; } = "";
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExperationDate { get; set; } = "";
+
+    }
+}
---
+++ NotBot2/Models/CardPaymentDetailContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotBot2.Models
+{
+    public class CardPaymentDetailContext:DbContext
+    {
+        public CardPaymentDetailContext(DbContextOptions options) : base(options) { 
+
+        }
+
+        public DbSet<CardPaymentDetail> CardPaymentDetails { get; set; }
+
+    }
+}
